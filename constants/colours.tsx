e5e9f2d1ac174236d1f9c68c39ec31676842d3a2
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-09-07 22:05:11 +0100

    more

---
+++ constants/colours.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const appColours = StyleSheet.create({
+  backgroundColour: {
+    backgroundColor: '#c42217',
+  },
+  foregroundColour: {
+    color: '#eed847',
+  },
+});
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,8 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { BackButton } from '../components/BackButton';
-import Details from '../screens/details';
 import Overview from '../screens/overview';
 
 export type RootStackParamList = {
@@ -16,14 +14,7 @@ export default function RootStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Overview">
-        <Stack.Screen name="Overview" component={Overview} />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={({ navigation }) => ({
-            headerLeft: () => <BackButton onPress={navigation.goBack} />,
-          })}
-        />
+        <Stack.Screen name="Overview" component={Overview} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
