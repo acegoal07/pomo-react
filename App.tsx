59c1c70b35fcc34bdcb060c19941a29221c015a9
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-09-08 12:12:54 +0100

    update

--- App.tsx
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler';
-
-import RootStack from './navigation';
-
-export default function App() {
-  return <RootStack />;
-}
---
+++ app/+not-found.tsx
@@ -0,0 +1,31 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <View>
+        <Text style={styles.title}>This screen doesn't exist.</Text>
+        <Link href="/" style={styles.link}>
+          <Text style={styles.linkText}>Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    marginTop: 16,
+    paddingVertical: 16,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
