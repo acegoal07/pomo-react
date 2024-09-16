ded84e6a7775504cc24a7fa8bb661141f89ffc97
Alan <alancrane19@outlook.com>
2024-09-16 15:05:30 +0100

    fixed svg, app now works on ios and android

--- components/todos.tsx
+++ components/todos.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, StyleSheet, View, Pressable, FlatList } from 'react-native';
 import CheckBox from 'react-native-bouncy-checkbox';
+import Svg, { Path } from 'react-native-svg';
 
 import { accentColor, foregroundColor } from '~/constants/colours';
 
@@ -77,12 +78,12 @@ export default function Todos({ todoList, setTodoList }: Readonly<TodosProps>) {
         onPointerEnter={() => handleMouseEnter('add')}
         onPointerLeave={() => handleMouseLeave('add')}
         onPress={handleAddTodoPress}>
-        <svg
+        <Svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
           style={styles.addTodoButtonText}>
-          <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
-        </svg>
+          <Path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
+        </Svg>
       </Pressable>
       <FlatList data={todoList} renderItem={renderTodoItem} />
     </View>
