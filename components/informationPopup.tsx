0881b21dfe17ffebf3adf76beb5731a62ebc8e4b
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-09-17 02:26:59 +0100

    Modal tweaks
    - Adds modal sizing properly and makes it responsive
    - Fixes inputs in login popup
    - Tweaks the looks and position of modal close button slightly
    - Adds shadows to certain buttons across all the modals

--- components/informationPopup.tsx
+++ components/informationPopup.tsx
@@ -10,12 +10,44 @@ interface InformationPopupProps {
 }
 
 export default function IInformationPopup({ visible, onClose }: Readonly<InformationPopupProps>) {
+  const [buttonHover, setButtonHover] = React.useState<{
+    closeButton: boolean;
+  }>({
+    closeButton: false,
+  });
+
+  /**
+   * Handle the mouse entering the button and adds the hover effect
+   * @param {string} button The button that the mouse has entered
+   * @returns {void}
+   */
+  function handleMouseEnter(button: string) {
+    setButtonHover({ ...buttonHover, [button]: true });
+  }
+
+  /**
+   * Handle the mouse leaving the button and removes the hover effect
+   * @param {string} button The button that the mouse has left
+   * @returns {void}
+   */
+  function handleMouseLeave(button: string) {
+    setButtonHover({ ...buttonHover, [button]: false });
+  }
+
   return (
     <Modal animationType="fade" transparent visible={visible} onRequestClose={onClose}>
       <View style={styles.modalOverlay}>
         <View style={styles.modalContent}>
           <Text style={styles.modalTitle}>Information</Text>
-          <Pressable style={styles.closeButton} onPress={onClose}>
+          <Pressable
+            style={
+              buttonHover.closeButton
+                ? { ...styles.closeButton, ...styles.closeButtonHover }
+                : styles.closeButton
+            }
+            onPointerEnter={() => handleMouseEnter('closeButton')}
+            onPointerLeave={() => handleMouseLeave('closeButton')}
+            onPress={onClose}>
             <Svg viewBox="0 0 384 512" style={styles.closeButtonText} fill="white">
               <Path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
             </Svg>
@@ -35,11 +67,13 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     minWidth: 250,
+    maxWidth: 600,
     padding: 20,
     backgroundColor,
     borderRadius: 10,
     borderWidth: 5,
     borderColor: accentColor,
+    width: '90%',
   },
   modalTitle: {
     fontSize: 24,
@@ -48,12 +82,22 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   closeButton: {
-    top: 10,
-    right: 10,
-    padding: 2,
+    top: 18,
+    right: 18,
+    padding: 4,
     backgroundColor: accentColor,
     borderRadius: 5,
     position: 'absolute',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+  },
+  closeButtonHover: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 9,
   },
   closeButtonText: {
     height: 30,
--- components/loginPopup.tsx
+++ components/loginPopup.tsx
@@ -12,20 +12,64 @@ interface LoginPopupProps {
 export default function LoginPopup({ visible, onClose }: Readonly<LoginPopupProps>) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [buttonHover, setButtonHover] = React.useState<{
+    submitButton: boolean;
+    closeButton: boolean;
+  }>({
+    submitButton: false,
+    closeButton: false,
+  });
 
-  const handleLogin = () => {
-    // Handle login logic here
+  /**
+   * Handle the mouse entering the button and adds the hover effect
+   * @param {string} button The button that the mouse has entered
+   * @returns {void}
+   */
+  function handleMouseEnter(button: string) {
+    setButtonHover({ ...buttonHover, [button]: true });
+  }
+
+  /**
+   * Handle the mouse leaving the button and removes the hover effect
+   * @param {string} button The button that the mouse has left
+   * @returns {void}
+   */
+  function handleMouseLeave(button: string) {
+    setButtonHover({ ...buttonHover, [button]: false });
+  }
+
+  /**
+   * Reset the form and close the modal.
+   */
+  function handleFormResetClose() {
+    setUsername('');
+    setPassword('');
+    onClose();
+  }
+
+  /**
+   * Handle the login form submission.
+   */
+  function handleLogin() {
     console.log('Username:', username);
     console.log('Password:', password);
-    onClose();
-  };
+    handleFormResetClose();
+  }
 
   return (
-    <Modal animationType="fade" transparent visible={visible} onRequestClose={onClose}>
+    <Modal animationType="fade" transparent visible={visible} onRequestClose={handleFormResetClose}>
       <View style={styles.modalOverlay}>
         <View style={styles.modalContent}>
           <Text style={styles.modalTitle}>Login</Text>
-          <Pressable style={styles.closeButton} onPress={onClose}>
+          <Pressable
+            style={
+              buttonHover.closeButton
+                ? { ...styles.closeButton, ...styles.closeButtonHover }
+                : styles.closeButton
+            }
+            onPointerEnter={() => handleMouseEnter('closeButton')}
+            onPointerLeave={() => handleMouseLeave('closeButton')}
+            onPress={onClose}>
             <Svg viewBox="0 0 384 512" style={styles.closeButtonText} fill="white">
               <Path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
             </Svg>
@@ -44,7 +88,15 @@ export default function LoginPopup({ visible, onClose }: Readonly<LoginPopupProp
               onChangeText={setPassword}
               secureTextEntry
             />
-            <Pressable style={styles.submit} onPress={handleLogin}>
+            <Pressable
+              style={
+                buttonHover.submitButton
+                  ? { ...styles.submit, ...styles.submitButtonHover }
+                  : styles.submit
+              }
+              onPress={handleLogin}
+              onPointerEnter={() => handleMouseEnter('submitButton')}
+              onPointerLeave={() => handleMouseLeave('submitButton')}>
               <Text style={styles.submitText}>Submit</Text>
             </Pressable>
           </View>
@@ -63,12 +115,13 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     minWidth: 250,
+    maxWidth: 600,
     padding: 20,
     backgroundColor,
     borderRadius: 10,
     borderWidth: 5,
     borderColor: accentColor,
-    width: '50%',
+    width: '90%',
   },
   modalTitle: {
     fontSize: 24,
@@ -77,25 +130,33 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   closeButton: {
-    top: 10,
-    right: 10,
-    padding: 2,
+    top: 18,
+    right: 18,
+    padding: 4,
     backgroundColor: accentColor,
     borderRadius: 5,
     position: 'absolute',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+  },
+  closeButtonHover: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 9,
   },
   closeButtonText: {
     height: 30,
     width: 30,
   },
   form: {
-    width: '100%',
     flexDirection: 'column',
-    gap: 10,
+    gap: 15,
   },
   input: {
     padding: 10,
-    marginVertical: 5,
     backgroundColor: 'white',
     borderRadius: 5,
     width: '100%',
@@ -104,10 +165,20 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: accentColor,
     borderRadius: 5,
-    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+  },
+  submitButtonHover: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 9,
   },
   submitText: {
     color: 'white',
     fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
