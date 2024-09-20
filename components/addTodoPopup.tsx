b8cfdf11f3b1d3e9b3224edf0ce31e38b1f2f4fd
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-09-20 19:19:05 +0100

    Update addTodoPopup.tsx

--- components/addTodoPopup.tsx
+++ components/addTodoPopup.tsx
@@ -89,6 +89,7 @@ export default function AddTodoPopup({
             <TextInput
               style={styles.input}
               onChangeText={setTaskContent}
+              placeholder="Enter your task here..."
               value={taskContent}
               multiline
               numberOfLines={10}
